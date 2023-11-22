b39821b4f838a19a41f0629889750f993f886bc3
estefaniemily <128614741+estefaniemily@users.noreply.github.com>
2023-11-22 11:34:41 -0300

    ok

--- main.js
+++ main.js
@@ -2,19 +2,33 @@ function tocaSomAplausos(){
     document.querySelector ('#som_tecla_aplausos').play();
 }
 document.querySelector('tecla_aplausos').onclick=tocaSomAplausos;
+
 function tocaSomVaia(){
     document.querySelector('#som_tecla_vaia').play();
 }
 document.querySelector('tecla_vaia').onclick=tocaSomVaia;
+
 function tocaSomRisadas(){
     document.querySelector ('#som_tecla_risadas').play();}
     document.querySelector('tecla_risadas').onclick=tocaSomRisadas
+    
     function tocaSomErrado(){
         document.querySelector ('#som_tecla_errado').play();}
         document.querySelector('tecla_errado').onclick=tocaSomErrado;
+        
         function tocaSomGrilo(){
             document.querySelector ('#som_tecla_grilo').play();}
             document.querySelector('tecla_grilo').onclick=tocaSomGrilo;
+            
             function tocaSomTambores(){
                 document.querySelector ('#som_tecla_tambores').play();
+document.querySelector ('tecla_aplausos').onclick=tocaSomTambores;
+function tocaSomTrombeta () {
+    document.querySelector ('#som_tecla_trombeta').play();}
+    document.querySelector('tecla_trombeta').onclick=tocaSomTrombeta;
+    function tocaSomVitoria(){
+        document.querySelection('#som_tecla_vitoria').play();}
+        document.querySelection('tecla_vitoria').onclick=tocaSomVitoria;
 
+
+}
